7ed8cd4508ae2482592781fb941a621c900cb30f
zerts <zertsalov96@yandex.ru>
2022-02-03 19:20:58 +0000

    new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: ``,
+    description: `NFTs/tattoos/engineering`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
@@ -28,7 +28,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tabIcon.jpeg`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/pages/test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Seo from "../components/seo";
+
+const TestPage: React.FC = () => (
+  <div>
+    <Seo title="test page" description="test page description" />
+  </div>
+);
+
+export default TestPage;
